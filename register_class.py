e85c8295e139ee9f5347b491e8e0b03b43804990
jameela <jhamody@campus.haifa.ac.il>
2022-06-27 11:45:59 +0300

    commit register_class

--- register_class.py
+++ register_class.py
@@ -3,7 +3,7 @@ import pytest
 import selenium
 import time
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
@@ -32,19 +32,21 @@ def test_create_account(driver):
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         driver.delete_all_cookies()
         time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR, "#ContentPlaceholder1_C023_Col00 > div.sfContentBlock > div > a").click()
+        element = driver.find_element(By.CSS_SELECTOR,"#ContentPlaceholder1_C023_Col00 > div.sfContentBlock > div > a")
+        webdriver.ActionChains(driver).move_to_element(element).click(element).perform()
+        # driver.find_element(By.CSS_SELECTOR, "#ContentPlaceholder1_C023_Col00 > div.sfContentBlock > div > a").click()
         time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR, "#Email-1").send_keys('fhfdssjam@gmail.com')
+        driver.find_element(By.CSS_SELECTOR, "#Email-1").send_keys('ahmadhamody@gmail.com')
         time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR, "#Textbox-1").send_keys("jameela")
+        driver.find_element(By.CSS_SELECTOR, "#Textbox-1").send_keys("zaki")
         time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR, "#Textbox-2").send_keys("suliman")
+        driver.find_element(By.CSS_SELECTOR, "#Textbox-2").send_keys("adham")
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR, "#Textbox-3").send_keys("amazon")
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR, "#Country-1").send_keys("Isreal")
         time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR, "#Textbox-4").send_keys("0549212345")
+        driver.find_element(By.CSS_SELECTOR, "#Textbox-4").send_keys("0549216578")
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR, "#form--1 > div:nth-child(6) > div:nth-child(10) > button").click()
         time.sleep(3)
@@ -53,5 +55,6 @@ def test_create_account(driver):
         assert actual_url == expected_url
      except NoSuchElementException:
          print("error")
-
+     except WebDriverException:
+         print('registration-successful')
 
